b96546adae37be500169cd1909df9ea1298861d6
Wen Xiao <wennstyle@gmail.com>
2021-09-11 22:58:38 -0400

    commit tree 6a236e9f9f51bd1ed21910562d2e98d41f741874

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Write your solution in this file!
 const employee = {name: 'Wen', streetAddress: '11 Broadway'}
 
-function updateEmployeeWithKeyAndValue(stuff, key, value) {
-    const newObj = {...stuff};
-    newObj[key] = value;
+function updateEmployeeWithKeyAndValue(stuff, key, value) { //where stuff is an object
+    const newObj = {...stuff}; //using the spread method to clone the object stuff and assigning it to the object variable names newObj
+    newObj[key] = value; //the key value pair where the value entered will be the value of that new object's index (based on the key)
     return newObj;
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
+    employee[key] = value; //there's no spread method so whatever is entered in the key and value will change this (original) object
     return employee;
 }
 
